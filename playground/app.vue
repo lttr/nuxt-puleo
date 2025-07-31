70b4953c196f92536e61b0f57e4f9951d68a6de6
Lukas Trumm <lukas.trumm@gmail.com>
2025-07-31 09:17:03 +0200

    Add example and missing deps

--- playground/app.vue
+++ playground/app.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>Nuxt module playground!</div>
+  <div class="p-container p-flow">
+    <h1>Hello world!</h1>
+    <p>This is a test. Puleo library is working!</p>
+    <h2>What a table</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Row 1, Column 1</td>
+          <td>Row 1, Column 2</td>
+        </tr>
+        <tr>
+          <td>Row 2, Column 1</td>
+          <td>Row 2, Column 2</td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>What a prose.</h2>
+    <div class="p-prose">
+      <h3>What a heading</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
+        sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
+        a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
+        molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
+      </p>
+      <h3>And another</h3>
+      <p>
+        In ac felis quis tortor malesuada pretium. Nunc feugiat mi a tellus
+        consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.
+        Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer
+        fringilla accumsan mi. Sed elementum fermentum wisi.
+      </p>
+    </div>
+  </div>
 </template>
-
-<script setup></script>

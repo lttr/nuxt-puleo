b208a21ff08f48acd1f2b9f8b82a187338f68c3a
Lukas Trumm <lukas.trumm@gmail.com>
2025-07-31 09:08:01 +0200

    Add my eslint and prettier config

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,18 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, createResolver } from "@nuxt/kit"
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+// export interface ModuleOptions {}
 
-export default defineNuxtModule<ModuleOptions>({
+export default defineNuxtModule({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: "nuxt-puleo",
+    configKey: "puleo",
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    _nuxt.options.css.push("@lttr/puleo")
   },
 })
